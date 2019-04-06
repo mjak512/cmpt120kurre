43d735c755fb53f8aa316f18c316be9298c4b775
mjak512 <mjkurre@hotmail.com>
2019-04-05 20:31:41 -0400

    arithmetic engine

---
+++ ArithmeticEngine.py
@@ -0,0 +1,44 @@
+def showIntro():
+        print("Welcome to the Arithmetic Engine!")
+        print("=================================\n")
+
+        
+def showOutro():
+        print("\nThank you for using the Arithmetic Engine…")
+        print("\nPlease come back again soon!")
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ")
+        cmd = cmd.lower()
+        
+        print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+
+        
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+        if cmd == "add":
+            result = num1 + num2
+        elif cmd == "sub":
+            result = num1 - num2
+        elif cmd == "mult":
+            result = num1 * num2
+        elif cmd == "div":
+            result = num1 // num2
+        elif cmd == "quit":
+             break
+            
+        else:
+            print("\n",cmd , "is not a valid command.")
+            break
+
+        print("The result is " + str(result) + ".\n")
+
+
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+
+
+main()
